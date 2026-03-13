4b3fd91330c7f6e2cf3146238fa6e5a04dfc8b9c
Pedro Piñera <pedro@pepicrft.me>
2026-03-13 16:16:29 +0100

    refactor: Simplify PiEx module, run integration tests by default
    - PiEx module now just delegates to Installer (install/installed?)
    - Points users to PiEx.Agent in moduledoc
    - Removed outdated session integration test
    - Integration tests now run by default (no exclude)

--- test/pi_ex/session_test.exs
+++ test/pi_ex/session_test.exs
@@ -1,7 +1,7 @@
 defmodule PiEx.SessionTest do
   use ExUnit.Case, async: true
 
-  alias PiEx.{Session, Tool}
+  alias PiEx.Session
 
   describe "child_spec/1" do
     test "returns valid child spec" do
@@ -17,29 +17,5 @@ defmodule PiEx.SessionTest do
 
       assert spec.id == :custom_id
     end
-  end
-
-  # Integration tests would require a running pi process
-  # These are marked as @tag :integration and can be run with:
-  # mix test --include integration
-
-  @tag :integration
-  @tag timeout: 30_000
-  test "can start a session with global pi" do
-    # Skip if pi is not installed
-    if System.find_executable("pi") == nil do
-      flunk("pi CLI not installed globally - skipping integration test")
-    end
-
-    {:ok, session} = Session.start_link(
-      api_key: System.get_env("ANTHROPIC_API_KEY") || "test-key"
-    )
-
-    # Wait for initialization
-    Process.sleep(1000)
-
-    assert Process.alive?(session)
-
-    Session.stop(session)
   end
 end
